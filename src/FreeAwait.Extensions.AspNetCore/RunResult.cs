1ec7ece4b84be686b28f7940bdb4476bece59796
yk <yuretz@gmail.com>
2021-11-22 22:09:49 +0100

    Added Asp.NET6.0 target for extensions and minimal web API result helper classes

---
+++ src/FreeAwait.Extensions.AspNetCore/RunResult.cs
@@ -0,0 +1,31 @@
+﻿#if NET6_0_OR_GREATER
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FreeAwait 
+{
+    public class RunResult : IResult
+    {
+        public RunResult(IStep step)
+        {
+            _step = step;
+        }
+
+        public async Task ExecuteAsync(HttpContext httpContext)
+        {
+            var runner = httpContext.RequestServices.GetRequiredService<IServiceRunner>();
+            var value = await _step.Use(runner);
+            var result = (value as IResult) ?? Results.Ok(value);
+            await result.ExecuteAsync(httpContext);
+        }
+
+        private readonly IStep _step;
+    }
+
+    public static class ResultExtensions
+    {
+        public static IResult Run(this IResultExtensions _, IStep step) => new RunResult(step);
+    }
+}
+#endif
